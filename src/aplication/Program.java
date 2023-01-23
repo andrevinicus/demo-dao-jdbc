081344cc22dae8cf85d2bc5c730b38b1c74288bb
andre <andre.gorlin@gmail.com>
2023-01-23 15:54:40 -0300

    delete implemetation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -3,6 +3,7 @@ package aplication;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -11,6 +12,7 @@ import model.entities.Seller;
 public class Program {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.creatSallerDao();
         
@@ -43,6 +45,13 @@ public class Program {
         seller.setName("Marta Waine");
         sellerDao.update(seller);
         System.out.println("Update completado");
+        
+        System.out.println("\n=== TESTE 6: seller Inset ====");
+        System.out.println("Enter id for delete teste: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed ");
+        sc.close();
     }
         
 }
